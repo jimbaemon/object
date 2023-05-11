8e8152a40e390a4deabb8e87e4b73a0e403fea6a
jimbaemon <jimbae@kakao.com>
2023-05-11 20:19:54 +0900

    init

---
+++ src/main/java/com/jimbae/book/obejct/chapter1/first/Ticket.java
@@ -0,0 +1,10 @@
+package com.jimbae.book.obejct.chapter1.first;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
---
+++ src/main/java/com/jimbae/book/obejct/chapter1/first/TicketSeller.java
@@ -0,0 +1,15 @@
+package com.jimbae.book.obejct.chapter1.first;
+
+public class TicketSeller {
+
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+
+}
